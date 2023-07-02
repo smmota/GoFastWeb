6fb8ee697813051d8229b8bc1365df51a628f8bb
Samuel Matos <s.mota1989@hotmail.com>
2023-07-02 19:15:51 -0300

    commit

---
+++ GoFast.UI/GoFast.UI/Shared/DTO/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFast.UI.Shared.DTO
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
